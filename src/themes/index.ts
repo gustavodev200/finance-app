2b4ae920760d4bca012b6992eec54d7c6156a58f
gustavodev200 <gustavolage200@gmail.com>
2022-09-25 15:56:08 -0300

    Iniciando App Finance APP

---
+++ src/themes/index.ts
@@ -0,0 +1,36 @@
+export const THEME = {
+  COLORS: {
+    BACKGROUND_900: "#180058",
+    BACKGROUND_800: "#211777",
+    BACKGROUND_700: "#C5C5C5",
+    BACKGROUND_600: "#D8D8D8",
+
+    TEXT: "#FFFFFF",
+
+    CAPTION_500: "#909090",
+    CAPTION_400: "#A1A1AA",
+    CAPTION_300: "#D4D4D8",
+
+    SHAPE: "#2A2634",
+
+    PRIMARY: "#FFCB26",
+    SUCCESS: "#34D399",
+    ALERT: "#F87171",
+
+    FOOTER: ["rgba(0,0,0,0)", "rgba(0,0,0,0.9)"],
+    OVERLAY: "rgba(0,0,0,0.6)",
+  },
+
+  FONT_FAMILY: {
+    REGULAR: "Inter_400Regular",
+    SEMI_BOLD: "Inter_600SemiBold",
+    BOLD: "Inter_700Bold",
+    BLACK: "Inter_900Black",
+  },
+
+  FONT_SIZE: {
+    SM: 14,
+    MD: 16,
+    LG: 24,
+  },
+};
